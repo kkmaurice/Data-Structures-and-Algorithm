4a8778f84377ca5626fb31d7475c129b3cf56b23
kkmaurice <adonyomaurice@gmail.com>
2023-10-10 09:39:41 +0300

    first commit

---
+++ linkedList.py
@@ -0,0 +1,60 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        if temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+            # self.length = 1
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            pre = self.head
+            while (temp.next):
+                pre = temp
+                temp = temp.next
+            self.tail = pre
+            self.tail.next = None
+            self.length -= 1
+            if self.length == 0:
+                self.head = None
+                self.tail = None
+            return temp.value
+
+
+my_linked_list = LinkedList(1)
+
+my_linked_list.append(2)
+
+# my_linked_list.print_list()
+# print(my_linked_list.print_list())
+
+print(my_linked_list.pop())
+
+print(my_linked_list.pop())
+
+print(my_linked_list.pop())

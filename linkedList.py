ed8a50ca70333dab03947e3211ddf5b8b4639ae4
kkmaurice <adonyomaurice@gmail.com>
2023-10-19 05:44:58 +0300

    finished linked list

--- linkedList.py
+++ linkedList.py
@@ -45,16 +45,75 @@ class LinkedList:
                 self.tail = None
             return temp.value
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        else:
+            temp = self.head
+            self.head = self.head.next
+            temp.next = None
+            self.length -= 1
+
+        if self.length == 0:
+            self.tail = None
+        return temp.value
+
+    def get(self, index):
+        if index < 0 or index > self.length:
+            return None
+        else:
+            temp = self.head
+            for _ in range(index):
+                temp = temp.next
+            return temp.value
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp = value
+            return True
+        return False
+
+    # Reversing a linked List
+    def revers(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
 
 my_linked_list = LinkedList(1)
-
 my_linked_list.append(2)
+my_linked_list.append(4)
+my_linked_list.append(7)
 
+my_linked_list.revers()
+
+my_linked_list.print_list()
+# print(my_linked_list.get(2))
 # my_linked_list.print_list()
 # print(my_linked_list.print_list())
 
-print(my_linked_list.pop())
+# print(my_linked_list.pop())
 
-print(my_linked_list.pop())
+# print(my_linked_list.pop())
 
-print(my_linked_list.pop())
+# print(my_linked_list.pop())
